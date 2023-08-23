2bb4f10e249d41c16d738f1abd80ef45269063af
Lovely <dongrelovely@gmail.com>
2023-08-23 15:10:31 +0530

    FYND Project
    Ecommerce mern stack app

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import cors from 'cors'
+import productRoutes from "./routes/productRoutes.js"
+import authRoutes from "./routes/authRoute.js"
+import categoryRoutes from './routes/categoryRoutes.js';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config();
+
+connectDB();
+const app=express();
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')));
+
+
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/category",categoryRoutes);
+app.use("/api/v1/product",productRoutes);
+
+
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+});
+
+// app.get('/',(req,res)=>{
+//     res.send("<h1>Welcome to ecommerce app</h1>");
+// })
+
+const PORT = process.env.PORT || 8080; 
+
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT}`);
+});
+
+
